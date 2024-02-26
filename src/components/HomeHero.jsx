f4ba58f33ad8590fc0e9efe20f88140777763703
Javohir0080703 <renobobopocta@gmail.com>
2024-02-26 20:26:31 +0500

    Initial commit

---
+++ src/components/HomeHero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeHero = () => {
+  return (
+    <section className='pt-[247px] pb-5 home-hero-bg'>
+        <div className='containerb'>
+
+        </div>
+    </section>
+  )
+}
+
+export default HomeHero
